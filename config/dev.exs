5b976fce8b20b3a0fa44b6710757189ddd276d81
Elvandro <Elvisamuni22@gmail.com>
2022-05-04 11:34:44 +0300

    Modify app configs

--- config/dev.exs
+++ config/dev.exs
@@ -26,7 +26,8 @@ config :cytech, CytechWeb.Endpoint,
   secret_key_base: "FmlbESoDvQT+ovOFvAqExJ/LEV7+QPchxzdKfJexHbAV5zr0yRcB6w9nC2j5lqys",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
